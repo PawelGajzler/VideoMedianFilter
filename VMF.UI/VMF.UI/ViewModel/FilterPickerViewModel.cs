973eb04317ed7ec74ecebe59af6e193a1faf2292
Paweł Gajzler <pawel.gajzler.dev@gmail.com>
2017-10-17 20:38:45 +0200

    added filterpickerview and viewmodel stub

--- VMF.UI/VMF.UI/ViewModel/FilterPickerViewModel.cs
+++ VMF.UI/VMF.UI/ViewModel/FilterPickerViewModel.cs
@@ -1,13 +1,43 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using VMF.UI.Messages;
 
 namespace VMF.UI.ViewModel
 {
     public class FilterPickerViewModel : ViewModelBase
     {
+        private List<string> options = new List<string>();
+
+        public List<string> Options
+        {
+            get
+            {
+                return options;
+            }
+            set
+            {
+                options = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private ICommand convertVideoCommand;
+
+        public ICommand ConvertVideoCommand
+        {
+            get
+            {
+                return convertVideoCommand ?? (convertVideoCommand = new RelayCommand(() =>
+                {
+                    MessengerInstance.Send(new TraceLogMessage("Operation Started"));
+                }));
+            }
+        }
     }
 }
